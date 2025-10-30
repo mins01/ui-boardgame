4f922e7d1cc1b98a73321e22a076df356c8aa21c
mins01 <mins01.lycos.co.kr@gmail.com>
2025-10-30 16:19:42 +0900

    first

---
+++ chess/Chess.js
@@ -0,0 +1,47 @@
+class Chess{
+    board = null;
+    square = null;
+    selectedSymbol = null;
+    constructor(board){
+        this.board = board;
+        this.squares = board.querySelectorAll('.square-grid > div'); // 64개
+        this.symbols = board.querySelectorAll('.symbol-grid > div'); // 64개
+        this.addEventListenerForSquares();
+        this.addEventListenerForSymbols();
+    }
+    addEventListenerForSquares(){
+        this.squares.forEach((square,idx) => {
+            square.addEventListener('click',()=>{
+                this.setSelectedSymbolOrder(idx+1);
+            })
+        });
+    }
+    setSelectedSymbolOrder(order){
+        if(this.selectedSymbol){
+            this.selectedSymbol.dataset.order = order;
+            this.selectSymbol(null)
+        }
+    }
+    addEventListenerForSymbols(){
+        this.symbols.forEach((symbol,idx) => {
+            symbol.addEventListener('click',()=>{
+                this.selectSymbol(symbol)
+            })
+        });
+    }
+    selectSymbol(symbol){
+        this.symbols.forEach((symbol)=>{
+            symbol.classList.remove('active');
+        })
+        if(!symbol){
+            this.selectedSymbol = null;
+            return 
+        }
+        if(this.selectedSymbol===symbol){
+            this.selectedSymbol = null;
+            return;
+        }
+        this.selectedSymbol = symbol;
+        symbol.classList.add('active');
+    }
+}
